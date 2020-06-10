2e0d44ff0fc8c136ed0b5077e21c7389c3f0859b
Irina Zakharova <letter.zakharova@ya.ru>
2020-06-10 16:36:11 +0300

    start

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="Make request to searcher",
+    version="0.1",
+    packages=['make_request_to_searcher'],
+    scripts=["main"],
+    # metadata to display on PyPI
+    author="Irina Zakharova",
+    author_email="letter.zakharova@ya.ru",
+    description="This is an studying project",
+    classifiers=[
+        "License :: OSI Approved :: Python Software Foundation License"
+    ]
+)
